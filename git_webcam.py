aaea6dc33e6365e50e00ee184d27933f6363f2fd
ismailozcil <35311771+ismailozcil@users.noreply.github.com>
2024-08-01 14:14:39 +0300

    Update git_webcam.py

--- git_webcam.py
+++ git_webcam.py
@@ -387,6 +387,7 @@ class AffordanceAnalyzer:
         self.take_photo(imagename)
 
     def image_estimate(self, imagename='photo.jpg'):
+        pil_img = pil_im.open(imagename)
         crops = self.objectDetector.detect_objects(imagename)
         TINT_COLOR = (0, 0, 0)  # Black
         TRANSPARENCY = .55  # Degree of transparency, 0-100%
