d6346dc8a4275ba37ec624a9bfb2efca18e41bdb
ismailozcil <35311771+ismailozcil@users.noreply.github.com>
2024-08-01 15:10:46 +0300

    Update git_webcam.py

--- git_webcam.py
+++ git_webcam.py
@@ -24,52 +24,6 @@ from google.colab.output import eval_js
 from base64 import b64decode
 from IPython.display import Image
 
-
-class get_webcam:
-    def __init__(self, image_name= 'photo.jpg', quality= 0.8):
-        self.image_name = image_name
-        self.quality =quality
-        self.take_photo()
-        
-    def capture_webcam(self):
-        js = Javascript('''
-            async function takePhoto(quality) {
-                const div = document.createElement('div');
-                const capture = document.createElement('button');
-                capture.textContent = 'Capture';
-                div.appendChild(capture);
-
-                const video = document.createElement('video');
-                video.style.display = 'block';
-                const stream = await navigator.mediaDevices.getUserMedia({video: true});
-
-                document.body.appendChild(div);
-                div.appendChild(video);
-                video.srcObject = stream;
-                await video.play();
-
-                // Resize the output to fit the video element.
-                google.colab.output.setIframeHeight(document.documentElement.scrollHeight, true);
-
-                // Wait for Capture to be clicked.
-                await new Promise((resolve) => capture.onclick = resolve);
-
-                const canvas = document.createElement('canvas');
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
-                canvas.getContext('2d').drawImage(video, 0, 0);
-                stream.getVideoTracks()[0].stop();
-                div.remove();
-                return canvas.toDataURL('image/jpeg', quality);
-            }
-            ''')
-        display(js)
-        data = eval_js('takePhoto({})'.format(self.quality))
-        binary = b64decode(data.split(',')[1])
-        with open(self.image_name, 'wb') as f:
-            f.write(binary)
-
-
 # @title get image
 class SquarePad:
     def __call__(self, image):
